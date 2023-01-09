a526fd12ea938a020800ea67ee0e78e6b9d52ff1
Kasi GAJAVALLI <kasi.gajavalli@gmail.com>
2023-01-09 10:02:18 +0100

    google api code changed

--- papm/constants/constants.js
+++ papm/constants/constants.js
@@ -1,7 +1,7 @@
 export { apiUrl, apiGoogleKey, apiGeocodeUrl, mapCenter, apiStreetViewUrl };
 
-const apiUrl = "http://localhost:3000/api";
-const apiGoogleKey = "AIzaSyAD0eMYODlMLxnRvVPyCKZbeudncPqNh7A";
+const apiUrl = "https://papm-five.vercel.app/api";
+const apiGoogleKey = "AIzaSyDYc7i6AUVS9H-EjWf_iApCRIGrSFw38Hs";
 const apiGeocodeUrl = "https://maps.googleapis.com/maps/api/geocode/json";
 const apiStreetViewUrl = "https://maps.googleapis.com/maps/api/streetview"
 const mapCenter = {
